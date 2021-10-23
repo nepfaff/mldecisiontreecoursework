e948d965240821d3af41806f9b93562d08f181c2
lg519 <lg519@ic.ac.uk>
2021-10-23 17:43:48 +0100

    addressed MR issues

--- entropy/entropy.py
+++ entropy/entropy.py
@@ -1,5 +1,19 @@
 import numpy as np
-from entropy.information import evaluate_information
+import math
+
+
+def evaluate_information(probability: float) -> float:
+    """
+    Evaluates the information of measuring a Random variable X as outcome x.
+
+    :param probability: The probability of X=x (i.e. P(X=x)).
+    :return: number of bits of information due to measuring a Random variable X as outcome x  :
+        - information: the information of measuring a Random variable X as outcome x (bits).
+    """
+
+    information = -math.log(probability, 2)
+
+    return information
 
 
 def evaluate_entropy(y: np.ndarray) -> float:
@@ -16,8 +30,8 @@ def evaluate_entropy(y: np.ndarray) -> float:
     # evaluate the probability of each outcome y_i as (#y_i)/(#total outcomes)
     probabilities = unique_counts / len(y)
 
-    entropy = 0
-    for prob in probabilities:
-        entropy += prob * evaluate_information(prob)
+    entropy = np.sum(
+        probabilities * np.array(list(map(evaluate_information, probabilities)))
+    )
 
     return entropy
