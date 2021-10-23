9651c3af5dc0e8f8f622a504f76789701967fc2c
lg519 <lg519@ic.ac.uk>
2021-10-23 17:09:25 +0100

    python package and addressed some MR issues

--- entropy/entropy.py
+++ entropy/entropy.py
@@ -1,5 +1,5 @@
 import numpy as np
-from information import evaluate_information
+from entropy.information import evaluate_information
 
 
 def evaluate_entropy(y: np.ndarray) -> float:
@@ -7,24 +7,17 @@ def evaluate_entropy(y: np.ndarray) -> float:
     Evaluates the entropy of a Random variable Y.
 
     :param y: Class labels of shape (n,). Each label is an outcome of the random variable Y.
-    :return: a float entropy  :
+    :return: The Shannon's entropy of the random variable Y  :
         - entropy: the Shannon's entropy of the random variable Y.
     """
 
-    (unique_y, unique_counts) = np.unique(y, return_counts=True)
+    (_, unique_counts) = np.unique(y, return_counts=True)
 
     # evaluate the probability of each outcome y_i as (#y_i)/(#total outcomes)
     probabilities = unique_counts / len(y)
-    print(unique_y)
-    print(probabilities)
 
     entropy = 0
     for prob in probabilities:
         entropy += prob * evaluate_information(prob)
 
     return entropy
-
-
-if __name__ == "__main__":
-    y = np.array([1, 0, 1, 1, 2])
-    evaluate_entropy(y)
--- entropy/information.py
+++ entropy/information.py
@@ -6,8 +6,8 @@ def evaluate_information(probability: float) -> float:
     Evaluates the information of measuring a Random variable X as outcome x.
 
     :param probability: The probability of X=x (i.e. P(X=x)).
-    :return: a float inform  :
-        - information: the information of measuring a Random variable X as outcome x.
+    :return: number of bits of information due to measuring a Random variable X as outcome x  :
+        - information: the information of measuring a Random variable X as outcome x (bits).
     """
 
     information = -math.log(probability, 2)
