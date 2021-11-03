ef7b00a5ec5c3e33b45203933f428dc8570c18ff
nicholas-p1 <kontakt@nicholaspfaff.ch>
2021-11-03 15:36:34 +0000

    Revert changes to 'load_txt_data' that are no longer needed due to the removal of label mappings

--- data_loading/data_loading.py
+++ data_loading/data_loading.py
@@ -13,18 +13,18 @@ def load_txt_data(path: str, attribute_number: int) -> Tuple[np.ndarray, np.ndar
     :param attribute_number: The number of attributes that the data contains.
 
     :return: A tuple of (x, y):
-        - x: Contains the attributes. An array of shape (n, k) where n is the
+        - x: Contains the attributes. An array of shape (n, k) and type float where n is the
             number of instances in the txt file and k is the 'attribute_number'.
-        - y: Contains the indices to reconstruct the original array from the y_unique. An array of shape (n,).
+        - y: Contains the class labels corresponding to x. An array of shape (n,) and type int.
     """
 
     data = np.loadtxt(path)
 
     # Split data into attributes and labels
     x = data[:, :attribute_number]
-
-    # y_lables: a numpy array with shape (n, ), and each element is the label corresponding
-    # to the nth instance in x.
     y = data[:, -1]
+
+    # Change the label type from float to int
+    y = y.astype(np.int)
 
     return x, y
