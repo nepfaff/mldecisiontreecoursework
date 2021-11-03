be8709313e0112bc7a908fb8cda0a7e1436d125f
nicholas-p1 <kontakt@nicholaspfaff.ch>
2021-11-03 14:59:12 +0000

    Add unit tests for 'construct_confusion_matrix

--- evaluation/__init__.py
+++ evaluation/__init__.py
@@ -1,2 +1,6 @@
-from evaluation.evaluation import cross_validation, nested_cross_validation
 from evaluation.evaluation_metrics import Evaluation
+from evaluation.evaluation import (
+    cross_validation,
+    nested_cross_validation,
+    construct_confusion_matrix,
+)
--- test/test_evaluation.py
+++ test/test_evaluation.py
@@ -1,81 +1,45 @@
+from typing import Tuple, List
+
+import pytest
 import numpy as np
-from evaluation import cross_validation, nested_cross_validation, Evaluation
+
+from evaluation import construct_confusion_matrix
 
 
-x = np.array(
-    [
-        [-64, -56, -61, -66, -71, -82, -81],
-        [-68, -57, -61, -65, -71, -85, -85],
-        [-63, -60, -60, -67, -76, -85, -84],
-        [-61, -60, -68, -62, -77, -90, -80],
-        [-63, -65, -60, -63, -77, -81, -87],
-        [-64, -55, -63, -66, -76, -88, -83],
-        [-65, -61, -65, -67, -69, -87, -84],
-        [-61, -63, -58, -66, -74, -87, -82],
-        [-65, -60, -59, -63, -76, -86, -82],
-        [-62, -60, -66, -68, -80, -86, -91],
-        [-67, -61, -62, -67, -77, -83, -91],
-        [-65, -59, -61, -67, -72, -86, -81],
-        [-63, -57, -61, -65, -73, -84, -84],
-        [-66, -60, -65, -62, -70, -85, -83],
-        [-61, -59, -65, -63, -74, -89, -87],
-        [-67, -60, -59, -61, -71, -86, -91],
-        [-63, -56, -60, -62, -70, -84, -91],
-        [-60, -54, -59, -65, -73, -83, -84],
-        [-60, -58, -60, -61, -73, -84, -88],
-        [-62, -59, -63, -64, -70, -84, -84],
+@pytest.fixture
+def construct_confusion_matrix_data() -> Tuple[
+    List[np.ndarray], List[np.ndarray], List[np.ndarray]
+]:
+    """
+    :return: A tuple of (y_gold, y_prediction, confusion_matrices):
+        - y_gold: A list of actual class label arrays of shape (n,).
+        - y_prediction: A list of predicted class label arrays of shape (n,).
+        - confusion_matrices: A list of confusion matrices where columns are predicted
+            and rows are actual labels. The confusion matrices correspond to 'y_gold'
+            and 'y_predicted'.
+    """
+
+    y_gold = [np.array([0, 0, 0, 0, 1, 1, 1, 1]), np.array([0, 0, 1, 1, 2, 2, 3, 3])]
+    y_prediction = [
+        np.array([0, 0, 1, 0, 1, 0, 1, 0]),
+        np.array([0, 3, 2, 1, 2, 0, 3, 0]),
     ]
-)
-
-y_labels = np.array([1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 4, 4, 4, 4, 4])
-
-# Map labels to class indexes
-(_, y) = np.unique(y_labels, return_inverse=True)
-
-
-# Testing cross validaiton
-
-
-evaluated1 = cross_validation(x, y)
-print(evaluated1)
-
-"""
-print(evaluated1.confusion_matrix)
-print("precisions:", evaluated1.precisions)
-print("recalls:", evaluated1.recalls)
-print("accuracy:", evaluated1.accuracy)
-print("f1s:", evaluated1.f1s)
-"""
-
-# Testing evaluation
-
-
-confusion_matrix = np.array(
-    [
-        [4, 5, 6],
-        [2, 1, 9],
-        [12, 1, 0],
+    confusion_matrices = [
+        [[3, 1], [2, 2]],
+        [[1, 0, 0, 1], [0, 1, 1, 0], [1, 0, 1, 0], [1, 0, 0, 1]],
     ]
-)
+
+    return y_gold, y_prediction, confusion_matrices
 
 
-evaluated2 = Evaluation(confusion_matrix)
-"""
-print(evaluated2.confusion_matrix)
-print("precisions:", evaluated2.precisions)
-print("recalls:", evaluated2.recalls)
-print("accuracy:", evaluated2.accuracy)
-print("f1s:", evaluated2.f1s)
-"""
+def test_construct_confusion_matrix(construct_confusion_matrix_data):
+    """
+    Tests 'construct_confusion_matrix'.
+    """
 
-
-# Testing nested cross validation
-
-nested_evaluated = nested_cross_validation(x, y)
-
-
-print(nested_evaluated.confusion_matrix)
-print("precisions:", nested_evaluated.precisions)
-print("recalls:", nested_evaluated.recalls)
-print("accuracy:", nested_evaluated.accuracy)
-print("f1s:", nested_evaluated.f1s)
+    y_gold, y_prediction, confusion_matrices = construct_confusion_matrix_data
+    for i in range(len(y_gold)):
+        assert np.all(
+            construct_confusion_matrix(y_gold[i], y_prediction[i])
+            == confusion_matrices[i]
+        ), f"test case {i} failed"
